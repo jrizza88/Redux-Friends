102891f392504971f46ba9bf5ddca2330879ee38
jrizza88 <jetorres88@gmail.com>
2019-02-05 20:20:47 -0500

    completed a dummy boilerplate

---
+++ friends/src/Reducers/Reducers.js
@@ -0,0 +1,19 @@
+
+export default function rootReducer () {
+    return{
+        title: 'Jamar\'s world'
+    }
+}
+
+// const initialState = {
+//     title: []
+// }
+
+// export function rootReducer(state = initialState, action){
+//   switch  (action.type){
+//       case hello:
+//   return console.log('dummy');
+//   default: 
+//   return state;
+//   }
+// }
--- friends/src/index.js
+++ friends/src/index.js
@@ -7,10 +7,10 @@ import { Provider } from 'react-redux';
 import {applyMiddleware, createStore} from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+import rootReducer from './Reducers/Reducers'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+rootReducer, applyMiddleware(thunk, logger)
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
