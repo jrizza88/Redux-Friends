feb7e08a10b9baeb4c1bb349cf35fd86b8c79aae
jrizza88 <jetorres88@gmail.com>
2019-02-06 23:40:26 -0500

    building FriendInput for the axios.post call

---
+++ friends/src/Components/FriendInput.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+class FriendInput extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            addFriend: []
+        }
+    }
+
+    
+}
